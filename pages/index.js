8ddcd3f2ebbe98886111e02bddb8d531f5661a76
matthewfelixr <matthewfelix88@gmail.com>
2022-03-20 21:25:58 +0700

    Environtment Commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+//our-domain.com/news
+// import Nav from 'react-bootstrap/Nav'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+<head>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap');
+    </style>
+</head>
+const Index = () => (
+    <>
+    <div>
+        <h1 className="text-center pt-4 web-title">bro.di</h1>
+    </div>
+    </>
+)
+
+export default Index;
