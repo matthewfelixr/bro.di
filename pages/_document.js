b03d16ab0bc2ad7bbb2942aa64c56e03d8e3a877
matthewfelixr <matthewfelix88@gmail.com>
2022-05-15 23:10:02 +0700

    Fix Login and Register

--- pages/_document.js
+++ pages/_document.js
@@ -7,13 +7,13 @@ export default function Document(props) {
     {/* // Responsive meta tag
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     //  bootstrap CDN */}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossOrigin="anonymous" />
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
     {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/> */}
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet"/>
     </Head>
       
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossOrigin="anonymous"></script>
       <body>
         <Main />
         <NextScript />
