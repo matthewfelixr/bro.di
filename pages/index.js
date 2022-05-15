1f7fb8fa612612ccbf93fd3777af48917d3230ce
matthewfelixr <matthewfelix88@gmail.com>
2022-05-16 00:05:18 +0700

    Fix Login and Register (8)

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,18 @@
 import React from 'react'
-import { Provider } from 'react-redux'
 import Layout from '../Layouts/Layout'
 import AboutUs from './components/Layout/AboutUs'
 import HeroSection from './components/Layout/HeroSection'
 import MitraKami from './components/Layout/MitraKami'
-import store from './components/Layout/store/store'
 // import 'bootstrap/dist/css/bootstrap.min.css';    
 
 
 const index = () => {
   return (
-    <Provider store={store}>
     <Layout>
         <HeroSection/>
         <AboutUs/>
         <MitraKami/>
     </Layout>
-    </Provider>
   )
 }
 
