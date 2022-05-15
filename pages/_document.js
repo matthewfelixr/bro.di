a0e83e06341eee6699aab8b320adbcd057a24423
matthewfelixr <matthewfelix88@gmail.com>
2022-05-15 22:23:07 +0700

    Fix Login and Register

--- pages/_document.js
+++ pages/_document.js
@@ -10,20 +10,20 @@ export default function Document(props) {
     <link
     href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-    crossorigin="anonymous" 
+    crossOrigin="anonymous" 
     />
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet"/>
     </Head>
       
     <Script
     src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-    crossorigin="anonymous"/>
+    crossOrigin="anonymous"/>
       <body>
         <Main />
-        <NextScript />
+        {/* <NextScript /> */}
       </body>
     </Html>
   );
